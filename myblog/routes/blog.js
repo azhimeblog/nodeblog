6cb56de834134af3141003752d3ab635da565987
azhimeblog <azhime.blog@gmail.com>
2020-11-11 11:18:14 +0700

    update ep6
    update ep6

--- myblog/routes/blog.js
+++ myblog/routes/blog.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const Blogs = require('../models/blogs')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -8,6 +9,10 @@ router.get('/', function(req, res, next) {
 
 router.get('/add', function(req, res, next) {
     res.render('blogs/addblog', { title: "เพิ่มข้อมูลบทความ" });
+});
+
+router.post('/add', function(req, res, next) {
+    console.log(req.body.title);
 });
 
 module.exports = router;
